a6ef49cc9a5d1d488a3c114cab81af321c2e62d5
Demian_Tatarinov <106443994+DemianTatarinov@users.noreply.github.com>
2025-03-11 09:20:14 +0100

    Update get_chat_id.py

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,17 +1,13 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler
+import requests
 
 TOKEN = '7810257621:AAGJaCdniU-1bphCkXV2wNFr-pJtIyqXBO8'
+URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 
-def start(update: Update, context):
-    chat_id = update.message.chat_id
-    update.message.reply_text(f"Ваш chat_id: {chat_id}")
+response = requests.get(URL)
+data = response.json()
 
-if __name__ == '__main__':
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-
-    updater.start_polling()
-    updater.idle()
+if 'result' in data and len(data['result']) > 0:
+    chat_id = data['result'][0]['message']['chat']['id']
+    print(f"Ваш chat_id: {chat_id}")
+else:
+    print("Не удалось получить chat_id. Отправьте сообщение вашему боту и запустите скрипт снова.")
